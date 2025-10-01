53fde04705954f3e169e7d61dddffa91f10ccb09
IbrokhimSidikov <ibrokhim.sidikov@gmail.com>
2025-10-02 00:12:23 +0500

    new home

--- lib/features/home/pages/home.dart
+++ lib/features/home/pages/home.dart
@@ -16,6 +16,9 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final AuthManager _authManager = AuthManager(); // Use singleton instance
+  final ScrollController _scrollController = ScrollController();
+  double _scrollOffset = 0;
+  
   final List<_ModuleItem> modules = [
     _ModuleItem("Profile", Icons.person_outline, AppColors.cxPrimary, '/profile'),
     _ModuleItem("Attendance", Icons.calendar_today_outlined, AppColors.cxSuccess, '/attendance'),
@@ -75,6 +78,22 @@ class _HomeState extends State<Home> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        _scrollOffset = _scrollController.offset;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.cxSoftWhite,
@@ -126,21 +145,28 @@ class _HomeState extends State<Home> {
 
             SizedBox(height: 24.sp),
 
-            // Grid of Modules
+            // Apple-Style Tiles
             Expanded(
-              child: GridView.builder(
+              child: ListView.builder(
+                controller: _scrollController,
+                padding: EdgeInsets.only(top: 8.sp, bottom: 32.sp),
                 itemCount: modules.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 20.sp,
-                  crossAxisSpacing: 20.sp,
-                  childAspectRatio: 1,
-                ),
                 itemBuilder: (context, index) {
-                  final module = modules[index];
-                  return _DashboardCard(
-                    module: module,
-                    onTap: () => _navigateToModule(module),
+                  // Calculate parallax effect
+                  final itemPosition = index * 120.h;
+                  final difference = _scrollOffset - itemPosition;
+                  final parallaxOffset = difference * 0.15;
+                  
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 16.sp),
+                    child: Transform.translate(
+                      offset: Offset(0, -parallaxOffset),
+                      child: _AppleTile(
+                        module: modules[index],
+                        index: index,
+                        onTap: () => _navigateToModule(modules[index]),
+                      ),
+                    ),
                   );
                 },
               ),
@@ -162,53 +188,215 @@ class _ModuleItem {
   _ModuleItem(this.title, this.icon, this.color, this.route);
 }
 
-// Dashboard Card Widget
-class _DashboardCard extends StatelessWidget {
+// Apple-Style Tile Widget
+class _AppleTile extends StatefulWidget {
   final _ModuleItem module;
+  final int index;
   final VoidCallback onTap;
 
-  const _DashboardCard({required this.module, required this.onTap});
+  const _AppleTile({
+    required this.module,
+    required this.index,
+    required this.onTap,
+  });
+
+  @override
+  State<_AppleTile> createState() => _AppleTileState();
+}
+
+class _AppleTileState extends State<_AppleTile> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  bool _isPressed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.97).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(18.r),
-          gradient: LinearGradient(
-            colors: [module.color.withOpacity(0.9), module.color.withOpacity(0.6)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: module.color.withOpacity(0.3),
-              blurRadius: 10,
-              offset: Offset(4, 6),
-            )
-          ],
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(16.sp),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(module.icon, size: 40.sp, color: AppColors.cxWhite),
-              SizedBox(height: 12.sp),
-              Text(
-                module.title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: AppColors.cxWhite,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w600,
-                ),
+      onTapDown: (_) {
+        setState(() => _isPressed = true);
+        _controller.forward();
+      },
+      onTapUp: (_) {
+        setState(() => _isPressed = false);
+        _controller.reverse();
+        Future.delayed(Duration(milliseconds: 100), widget.onTap);
+      },
+      onTapCancel: () {
+        setState(() => _isPressed = false);
+        _controller.reverse();
+      },
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Hero(
+          tag: 'module_${widget.index}',
+          child: Container(
+            height: 110.h,
+            margin: EdgeInsets.symmetric(horizontal: 4.sp),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.r),
+              gradient: LinearGradient(
+                colors: [
+                  widget.module.color.withOpacity(0.92),
+                  widget.module.color.withOpacity(0.88),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
-            ],
+              boxShadow: [
+                BoxShadow(
+                  color: widget.module.color.withOpacity(0.25),
+                  blurRadius: 16,
+                  offset: Offset(0, 8),
+                  spreadRadius: -3,
+                ),
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.08),
+                  blurRadius: 24,
+                  offset: Offset(0, 12),
+                  spreadRadius: -6,
+                ),
+              ],
+            ),
+            child: Stack(
+              children: [
+                // Subtle background pattern
+                Positioned.fill(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.r),
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.white.withOpacity(0.08),
+                          Colors.transparent,
+                          Colors.black.withOpacity(0.05),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                  ),
+                ),
+                
+                // Decorative corner element
+                Positioned(
+                  top: -20,
+                  right: -20,
+                  child: Container(
+                    width: 80.sp,
+                    height: 80.sp,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.1),
+                    ),
+                  ),
+                ),
+                
+                // Content
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.sp, vertical: 16.sp),
+                  child: Row(
+                    children: [
+                      // Icon with Apple-style minimal background
+                      Container(
+                        width: 56.sp,
+                        height: 56.sp,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(14.r),
+                          color: Colors.white.withOpacity(0.2),
+                          border: Border.all(
+                            color: Colors.white.withOpacity(0.15),
+                            width: 0.5,
+                          ),
+                        ),
+                        child: Icon(
+                          widget.module.icon,
+                          size: 30.sp,
+                          color: Colors.white,
+                        ),
+                      ),
+                      
+                      SizedBox(width: 16.sp),
+                      
+                      // Text content - Apple-style typography
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.module.title,
+                              style: TextStyle(
+                                fontSize: 20.sp,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                                letterSpacing: -0.3,
+                                height: 1.2,
+                              ),
+                            ),
+                            SizedBox(height: 4.sp),
+                            Text(
+                              _getSubtitle(widget.module.title),
+                              style: TextStyle(
+                                fontSize: 13.sp,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white.withOpacity(0.85),
+                                letterSpacing: -0.1,
+                                height: 1.3,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      
+                      // Chevron indicator
+                      Icon(
+                        Icons.chevron_right_rounded,
+                        color: Colors.white.withOpacity(0.6),
+                        size: 24.sp,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  String _getSubtitle(String title) {
+    switch (title) {
+      case "Profile":
+        return "Personal information";
+      case "Attendance":
+        return "Work hours & tracking";
+      case "Break Records":
+        return "Meal history";
+      case "History":
+        return "Activity log";
+      case "Achievements":
+        return "Your rewards";
+      default:
+        return "Tap to explore";
+    }
   }
 }
